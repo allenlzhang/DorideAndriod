85c7a31d1ccaa97087592e162ac0a463fa51ccc7
zhanglei <allen.zhang@carlt.com.cn>
2018-07-11 10:56:35 +0800

    1，//mViewError.setVisibility(View.VISIBLE);

--- app/src/main/java/com/carlt/doride/base/BaseFragment.java
+++ app/src/main/java/com/carlt/doride/base/BaseFragment.java
@@ -171,7 +171,7 @@ public abstract class BaseFragment extends Fragment {
         }
         mTxtRetryError.setVisibility(View.VISIBLE);
         mTxtEorrorSub.setText(info);
-        mViewError.setVisibility(View.VISIBLE);
+//        mViewError.setVisibility(View.VISIBLE);
     }
 
     @Override
